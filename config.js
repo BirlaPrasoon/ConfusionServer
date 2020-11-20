e3b64744a993eabf894b6aa4e30f5f4707667842
Prasoon Birla <prasoon.birla@whatfix.com>
2020-11-20 19:09:55 +0530

    uploading on github

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': 'xxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'mongoUrl' : 'xxxxxxxxxxxxxxxxxxxxxx',
+    'facebook': {
+        clientId: 'xxxxxxxxxxxxxxxx',
+        clientSecret: 'xxxxxxxxxxxxxxxxxxxx'
+    }
+}
