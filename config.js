3eed52d83c5cd86f49a1348bd3f057dbc7e91927
Prasoon Birla <prasoon.birla@whatfix.com>
2020-11-21 15:52:32 +0530

    Adding missing files related to Secure Server

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'secretKey': 'xxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    'mongoUrl' : 'xxxxxxxxxxxxxxxxxxxxxx',
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: 'xxxxxxxxxxxxxxxx',
-        clientSecret: 'xxxxxxxxxxxxxxxxxxxx'
+        clientId: '254763648659343',
+        clientSecret: '0e4ce1367c7a1bdebc5bbb1a150f7f32'
     }
 }
