2fb90a29e7e4aaccaa3e820e89878e6184142655
Prasoon Birla <birla.prasoon1@gmail.com>
2020-11-23 08:30:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '254763648659343',
-        clientSecret: '0e4ce1367c7a1bdebc5bbb1a150f7f32'
+        clientId: '',
+        clientSecret: ''
     }
 }
